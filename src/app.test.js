821dc85b94e8cc91a35c25d2219b879caef4ad81
AkielCh <akiel98@hotmail.com>
2023-09-01 17:16:18 +0100

    Test to check that canAddLetter returns false if letter is in the same subarray of last letter of word

--- src/app.test.js
+++ src/app.test.js
@@ -5,7 +5,7 @@
 const app = require('./app');
 
 describe('When given a word',()=>{
-  it('should allow you to add any letter from a different array',()=>{
+  it('should allow you to add any letter from a different subarray',()=>{
     let grid =[
       ["L","R","K"],
       ["A","T","H"],
@@ -21,3 +21,21 @@ describe('When given a word',()=>{
   })
 })
 
+
+
+describe('When given a word',()=>{
+  it('should prevent the adding of any letter from the same subarray',()=>{
+    let grid =[
+      ["L","R","K"],
+      ["A","T","H"],
+      ["Z","E","D"],
+      ["I","O","C"],
+    ]
+
+    const word ="RA";
+    const letter = "T";
+    const result = app.canAddLetter(grid,word,letter);
+    expect(result).toBe(false)
+
+  })
+})
